f87f6432b2cc11d27bdeb920c30d76ec66130a21
Tibor Kircsi <tkircsi@gmail.com>
2023-03-05 10:01:27 +0100

    implement add bridge

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/tkircsi/custom-cni
 
 go 1.19
 
-require github.com/containernetworking/cni v1.1.2 // indirect
+require (
+	github.com/containernetworking/cni v1.1.2 // indirect
+	github.com/vishvananda/netlink v1.1.0 // indirect
+	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,47 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"syscall"
+
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/version"
+	"github.com/vishvananda/netlink"
 )
+
+type SimpleBridge struct {
+	BridgeName string `json:"bridgeName"`
+	IP         string `json:"ip"`
+}
 
 func main() {
 	skel.PluginMain(add, check, del, version.All, "about custom-cni plugin")
 }
 
 func add(args *skel.CmdArgs) error {
+	sb := SimpleBridge{}
+	if err := json.Unmarshal(args.StdinData, &sb); err != nil {
+		return err
+	}
+	fmt.Printf("%v\n", sb)
+
+	br := &netlink.Bridge{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:   sb.BridgeName,
+			MTU:    1500,
+			TxQLen: -1,
+		},
+	}
+
+	err := netlink.LinkAdd(br)
+	if err != nil && err != syscall.EEXIST {
+		return err
+	}
+
+	if err := netlink.LinkSetUp(br); err != nil {
+		return err
+	}
 	return nil
 }
 
